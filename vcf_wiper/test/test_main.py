ff0247fa960934d3c6f0b4978a8e0d892d6e87d4
Alessandro Guida <alessando-guida@users.noreply.github.com>
2021-12-30 09:39:14 -0400

    add pytest and InfoHeader

---
+++ vcf_wiper/test/test_main.py
@@ -0,0 +1,15 @@
+import vcf_wiper
+from vcf_wiper import wiper
+import os
+#from click.testing import CliRunner
+
+
+print("ROOT_DIR: %s" % vcf_wiper.ROOT_DIR)
+
+# define test vcf file
+test_input = os.path.join(vcf_wiper.ROOT_DIR, "../data/VCFv4.2.vcf")
+assert os.path.isfile(test_input), "File not found: %s" % test_input
+
+
+wiper.wipe_vcf(vcf_in=test_input, vcf_out="deleteme.vcf")
+#wiper.wipe_vcf(test_input)
